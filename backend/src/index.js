9a302f8a9108622842d2c07ad1108b077b1783c3
Vlad <vlad-mihai.ene@student.manchester.ac.uk>
2020-06-18 16:34:01 +0100

    Add parser endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,38 +7,71 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/solve", (req, res) => {
-  console.log(req.body);
+  console.log(`Solve: ${req.body.clauses}`);
   res.status(200).json(vampireSolve(req.body.clauses, req.body.args));
+  console.log(`Finished solving`);
+})
+
+app.post("/parse", (req, res) => {
+  console.log(`Parse: ${req.body.clauses}`);
+  res.status(200).json(vampireParse(req.body.clauses));
+  console.log(`Finished parsing`);
 })
 
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 })
 
-function getErrorLines(str){
-  const regex = /on line ([0-9]*)/ig;
-  let result;
-  let lines = []
-  while (result = regex.exec(str))
-    lines.push(result[1]);
-  return lines;
+function parseErrorMessage(str) {
+  const regex = /on line ([0-9]*)\n(.*)\n/ig;
+  let result = regex.exec(str);
+  if (result) {
+    return {
+      line: result[1],
+      text: result[2]
+    };
+  }
+  else {
+    return {};
+  }
+}
+
+function argsToString(args) {
+  console.log(args);
+  let str = "";
+  for (let [key, value] of Object.entries(JSON.parse(args))) {
+    if (typeof value === 'boolean') str += `${key} `;
+    else str += `${key} ${value} `;
+  }
+  console.log(str);
+  return str;
+}
+
+function vampireParse(clauses) {
+  try {
+    execSync(`echo ${clauses} | ./vampire-executables/vampire4.2.2 --mode output`)
+    return {
+      error: {}
+    }
+  }
+  catch (error) {
+    return { error: parseErrorMessage(error.stdout) };
+  }
 }
 
 function vampireSolve(clauses, args) {
   try {
-    const solution = execSync(`echo ${clauses} | ./vampire-executables/vampire4.2.2 ${args}`).toString()
+    const stringArgs = argsToString(args);
+    const solution = execSync(`echo ${clauses} | ./vampire-executables/vampire4.2.2 ${stringArgs}`).toString()
     return {
       rawOutput: `${solution}`
     };
   }
   catch (error) {
     console.log(`Error: ${error.stdout}\nstderr: ${error.stderr}\nstdout: ${error.stdout}`);
-    const regex = /on line ([0-9]*)/ig;
     return {
       rawOutput: `${error.stdout}`,
-      errors: {
-        lines: getErrorLines(error.stdout)
-      }
+      error: parseErrorMessage(error.stdout)
     };
   }
 }

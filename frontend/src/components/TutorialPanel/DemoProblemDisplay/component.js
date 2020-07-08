308f30b7c143588c35c064511a8b9809ac8e5cd6
Vlad <vlad-mihai.ene@student.manchester.ac.uk>
2020-07-08 02:54:36 +0100

    Add tutorial demo

---
+++ frontend/src/components/TutorialPanel/DemoProblemDisplay/component.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ExecutionBar from '../../ExecutionBar/component';
+import { ExecutionContextProvider, ExecutionContext } from '../../../contexts/ExecutionContext';
+import Editor from '../Editor';
+
+
+const DemoProblemDisplay = (props) => {
+  const mainExecCtx = React.useContext(ExecutionContext);
+  return (
+    <div style={{ paddingLeft: "0.5rem", paddingRight: "0.5rem" }}>
+      <ExecutionContextProvider
+        overrideValues={{
+          output: mainExecCtx.output,
+          updateOutput: mainExecCtx.updateOutput
+        }}
+        defaultInput={props.defaultInput} >
+        <ExecutionBar tutorial style={{ margin: "0.1rem" }} />
+        <Editor type="input" disableParsingErrors={props.disableParsingErrors} />
+      </ExecutionContextProvider>
+    </div>
+  )
+}
+
+export default DemoProblemDisplay;

3da1e97967fa3a5d1d938ac5f43f2663809b96bb
Vlad <vlad-mihai.ene@student.manchester.ac.uk>
2020-07-19 15:54:13 +0100

    Refine restriction policies

--- backend/src/options_utils/restricted_options.js
+++ backend/src/options_utils/restricted_options.js
@@ -1,6 +1,15 @@
 const restriction_policies = [
   {
     userType: "sudo",
+    time_limit: {
+      minValue: 0,
+    },
+    memory_limit: {
+      minValue: 0,
+    },
+    cores: {
+      minValue: 0,
+    },
     options: {
       latex_output: true,
       ltb_directory: true,
@@ -11,12 +20,15 @@ const restriction_policies = [
     userType: "any",
     options: {
       time_limit: {
+        minValue: 0.01,
         maxValue: 60
       },
       memory_limit: {
+        minValue: 1,
         maxValue: 3000
       },
       cores: {
+        minValue: 1,
         maxValue: 1
       },
       latex_output: true,
